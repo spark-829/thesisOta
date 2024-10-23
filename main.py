40aef6fa1e0ed43804a343cf74f3735522ea13f2
spark-829 <vicsonyaris29@gmail.com>
2024-10-23 23:46:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-#it doesnt change wtf
-print("ttaadfafa")
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+
+firmware_url = "https://raw.githubusercontent.com/spark-829/thesisOta/"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+ota_updater.download_and_install_update_if_available()
